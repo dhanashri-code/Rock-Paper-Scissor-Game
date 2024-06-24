fd11aa472504c2bdab2d47e241908c9b7f02ff03
Dhanashri Bhavsar <129056367+dhanashri-code@users.noreply.github.com>
2024-06-24 13:47:46 +0530

    Update RockPaperGameGui.java

--- RockPaperGameGui.java
+++ RockPaperGameGui.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 class RockPaperGameGui extends JFrame{
 
+    /* Instance Member Variable */
     JLabel l1 , l2 , l3 ;
     JTextField l4 , l5 , t1;
     JLabel a1 , a2 , a3 , sP , pS ,  sA , aS;
@@ -12,14 +13,18 @@ class RockPaperGameGui extends JFrame{
     int ch , scP , scA;
     Random r = new Random();
     int ai;
+
+    /* Images For Player */
     ImageIcon i1 = new ImageIcon("C:/Users/Sarvadnya/Desktop/java/Pl_Rock.jpg");
     ImageIcon i2 = new ImageIcon("C:/Users/Sarvadnya/Desktop/Java/Pl_paper.jpg");
     ImageIcon i3 = new ImageIcon("C:/Users/Sarvadnya/Desktop/java/Pl_Scissor.jpg");
 
+    /* Images For Computer */
     ImageIcon ii1 = new ImageIcon("C:/Users/Sarvadnya/Desktop/java/Ai_Rock.jpg");
     ImageIcon ii2 = new ImageIcon("C:/Users/Sarvadnya/Desktop/java/Ai_paper.jpg");
     ImageIcon ii3 = new ImageIcon("C:/Users/Sarvadnya/Desktop/java/Ai_Scissor.jpg");
-       
+    
+    /* Constructor */
     RockPaperGameGui(String s1){
         super(s1);
     }
@@ -29,6 +34,7 @@ class RockPaperGameGui extends JFrame{
     
     void setCompo()
     {
+        /* Creating Objects */
         b1 = new JButton("Rock");
         b2 = new JButton("Paper");
         b3 = new JButton("Scissors");
@@ -39,7 +45,7 @@ class RockPaperGameGui extends JFrame{
         l3 = new JLabel();
 
         t1 = new JTextField("  Rock Paper Scissors Game ! ");
-        l4 = new JTextField(JTextField.CENTER);
+        l4 = new JTextField();
         l5 = new JTextField();
 
         t1.setEditable(false);
@@ -51,31 +57,30 @@ class RockPaperGameGui extends JFrame{
         a3 = new JLabel();
 
         sP = new JLabel("Player Score : ");
-        sA = new JLabel("AI Score : ");
+        sA = new JLabel("Computer Score : ");
         pS = new JLabel();
         aS = new JLabel();
         
+        /* For JTextFeild Text Align in Center */
         l4.setHorizontalAlignment(JTextField.CENTER);
         l5.setHorizontalAlignment(JTextField.CENTER);
 
+        /* Wait Cursor Effect */
         Cursor c1 = new Cursor(Cursor.WAIT_CURSOR);
         b4.setCursor(c1);
 
+        /* Hand Cursor Effect */
         Cursor c2 = new Cursor(Cursor.HAND_CURSOR);
         b1.setCursor(c2);
         b2.setCursor(c2);
         b3.setCursor(c2);
 
+        /* changing Font Style  */
         Font f1 = new Font("Callibri",Font.BOLD,15);
         sP.setFont(f1);
         pS.setFont(f1);
         sA.setFont(f1);
         aS.setFont(f1);
-
-        sP.setForeground(Color.WHITE);
-        pS.setForeground(Color.WHITE);
-        sA.setForeground(Color.WHITE);
-        aS.setForeground(Color.WHITE);
 
         Font f2 = new Font("Times New Roman",Font.BOLD,18);
         b1.setFont(f2);
@@ -91,11 +96,19 @@ class RockPaperGameGui extends JFrame{
         l4.setFont(f4);
         l5.setFont(f4);
 
+        /* Set Text Color of Score Bord */
+        sP.setForeground(Color.WHITE);
+        pS.setForeground(Color.WHITE);
+        sA.setForeground(Color.WHITE);
+        aS.setForeground(Color.WHITE);
+
+        /* set Backgrount-Color of Buttons */
         b1.setBackground(Color.PINK);
         b2.setBackground(Color.YELLOW);
         b3.setBackground(Color.CYAN);
         b4.setBackground(Color.LIGHT_GRAY);
         
+        /* adding componentes in Frame */
         add(t1);
         add(b1);
         add(b2);
@@ -117,6 +130,7 @@ class RockPaperGameGui extends JFrame{
         add(pS);
         add(aS);
 
+        /* position of Componenets */
         t1.setBounds(70,20,300,50);
 
         b1.setBounds(40,130,100,50);
@@ -126,12 +140,13 @@ class RockPaperGameGui extends JFrame{
       
         sP.setBounds(40,70,120,50);
         pS.setBounds(160,70,100,50);
-        sA.setBounds(250,70,100,50);
-        aS.setBounds(335,70,100,50);
+        sA.setBounds(240,70,140,50);
+        aS.setBounds(380,70,100,50);
 
         l4.setBounds(130,350,150,40);
         l5.setBounds(110,400,200,40);
 
+        /* Add ActionListener Interface on Buttons */
         b1.addActionListener(new B1());
         b2.addActionListener(new B1());
         b3.addActionListener(new B1());
@@ -142,6 +157,7 @@ class RockPaperGameGui extends JFrame{
     
 public static void main(String args[])
 {
+    /*Creating Frame */
     RockPaperGameGui rps = new RockPaperGameGui("Rock Paper Scissors Game.");
     rps.setVisible(true);
     rps.setSize(450,570);
@@ -151,50 +167,55 @@ public static void main(String args[])
     rps.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     rps.setLocationRelativeTo(null);
 }
-
+/* Implement ActionListener Interface */
 class B1 implements ActionListener
     {
-       
         public void actionPerformed(ActionEvent e1)
         {
+            /* Rock Button */
             if(e1.getSource() == b1)
             {
                 l2.setIcon(null);
                 l3.setIcon(null);
 
                 ch=1;
-
+                /* Player Rock Image */
                 l1.setBounds(60,170,300,200);
                 l1.setIcon(i1);
             }
 
+            /* Player Paper Button */
             else if(e1.getSource() == b2)
             {
                 l1.setIcon(null);
                 l3.setIcon(null);
 
                 ch=2;
-
+                /* Player Paper Image */
                 l2.setBounds(60,170,300,200);
                 l2.setIcon(i2);
-
             }
 
+            /* Scissor Button */
             else if(e1.getSource() == b3)
             {
                 l2.setIcon(null);
                 l1.setIcon(null);
 
                 ch=3;
-
+                /* Player Scissor Image */
                 l3.setBounds(60,170,300,200);
                 l3.setIcon(i3);
 
             }
 
+            /* Result Button */
             else if(e1.getSource() == b4)
             {
+                /* Random Image Generator */
                 ai = r.nextInt(1,4);
+
+                /* Computer Rock Image */
                 if(ai == 1)
                 {
                     a2.setIcon(null);
@@ -203,6 +224,8 @@ class B1 implements ActionListener
                     a1.setBounds(240,170,300,200);
                     a1.setIcon(ii1);
                 }
+
+                /* Computer Paper Image */
                 else if(ai == 2)
                 {
                 a3.setIcon(null);
@@ -212,6 +235,7 @@ class B1 implements ActionListener
                 a2.setIcon(ii2);
                 }
             
+                /* Computer Scissor Image */
                 else if(ai == 3)
                 {
                 a2.setIcon(null);
@@ -221,6 +245,7 @@ class B1 implements ActionListener
                 a3.setIcon(ii3);
                 }
 
+                /* possible Cases for Conditions to win / lose / tie */
                 switch(ch)
                 {
                     case 1:
@@ -237,8 +262,8 @@ class B1 implements ActionListener
                         l5.setVisible(true);
                         l4.setText(" Computer win ");
                         l5.setText(" Paper covers rock");
-                        scA++;
                         l4.setBackground(Color.RED);
+                        scA++;
                     }
 
                     else
@@ -308,9 +333,10 @@ class B1 implements ActionListener
                     break;
                     
                     default:
-                        l4.setText(" please Enter valid choice. ");
+                        l4.setText(" please Choose valid option. ");
            
                 }   
+                /* Convertin Interger Score to String */
                 pS.setText(Integer.toString(scP));
                 aS.setText(Integer.toString(scA));
             }
